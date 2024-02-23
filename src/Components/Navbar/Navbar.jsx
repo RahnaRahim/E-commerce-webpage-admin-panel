934172682fa579c8e4e89d4b10f47b04373591d3
unknown <rahnarahim611@gmail.com>
2024-02-23 12:57:55 +0530

    'port'

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+    
+      <p>SHOP<span style={{color:"brown"}}>IKO</span></p>
+    
+    </div>
+  )
+}
+
+export default Navbar
